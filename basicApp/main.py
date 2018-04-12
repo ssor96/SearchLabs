9fd44027e660c447f5347cf77f81c1e349befd28
ssor96 <sergey040496@yandex.ru>
2018-04-13 02:55:56 +0300

    Add background and icon. Added static files in gitignore

--- basicApp/main.py
+++ basicApp/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+import os
+from flask import Flask, request, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/search')
